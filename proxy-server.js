36ef4ac76e4a46a6d6663712b7e4b2299c5ad743
cobin <cobin@21cn.com>
2012-03-14 19:15:18 +0800

    --

--- proxy-server.js
+++ proxy-server.js
@@ -22,7 +22,7 @@ fs.writeFileSync(__dirname + '/proxy-server.pid', process.pid.toString(), 'ascii
 //自选股服务处理总接口，包含增加，删除，修改，下载，上传等动作
 app.get('/:fakecode/Stocks', function(req, res) {
 	_logger.info([ 'SelfStock', req.url ]);
-	res.header("Content-Type","application/json; charset=utf-8");	
+	res.header("Content-Type","application/json; charset=utf-8");
 	var sAction = req.query.action;
 	
 	if(sAction==='list'){ //查询自选股回应内容：600000,600001
@@ -34,10 +34,10 @@ app.get('/:fakecode/Stocks', function(req, res) {
 			selfstock.add(req,res);
 		}
 	}else if(sAction==='remove'){ //删除自选股
-		selfstock.delete(req,res);
+		selfstock.deletes(req,res);
 	}else{
 		res.send('{"error":"无效的请求","result":null}');
-	}	
+	}
 });
 
 app.listen(settings.selfport);
@@ -49,5 +49,5 @@ process.on('uncaughtException', function(e){
 });
 
 process.on('exit',function(){
-    console.log("Exit SelfStock Proxy Server.");	
+    console.log("Exit SelfStock Proxy Server.");
 });
